1ccbb1b7ee3365f6fa33fe62b84602628fec8530
Baktilek <baktilekashyrov8@gmail.com>
2021-01-21 23:14:59 +0600

    Добавил Интерфейс с одним методом

---
+++ RazorPagesLessons.Services/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RazorPagesLessons.Models;
+
+namespace RazorPagesLessons.Services
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAllEmployess();
+    }
+}
