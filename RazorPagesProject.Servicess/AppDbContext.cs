47dcbd559d1ffe35b58142017694c76d852bf9cc
Baktilek <baktilekashyrov8@gmail.com>
2021-01-28 10:35:47 +0600

    Добавил в проект все необходимые библиотеки чтобы раотать с БД

---
+++ RazorPagesProject.Servicess/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesProject.Modelss;
+
+namespace RazorPagesProject.Servicess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
